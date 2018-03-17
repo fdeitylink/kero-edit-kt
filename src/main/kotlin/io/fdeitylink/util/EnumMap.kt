189fb91c52c38e15ac9ce8c7706594c670c20927
fdeitylink <11952260+fdeitylink@users.noreply.github.com>
2018-03-17 19:32:49 -0400

    Added enumMapOf functions

---
+++ src/main/kotlin/io/fdeitylink/util/EnumMap.kt
@@ -0,0 +1,15 @@
+package io.fdeitylink.util
+
+import java.util.EnumMap
+
+/**
+ * Returns an empty new [EnumMap]
+ */
+inline fun <reified K : Enum<K>, V> enumMapOf() = EnumMap<K, V>(K::class.java)
+
+/**
+ * Returns a new [EnumMap] with the specified contents, given as a list of pairs
+ * where the first component is the key and the second is the value
+ */
+inline fun <reified K : Enum<K>, V> enumMapOf(vararg pairs: Pair<K, V>) =
+        EnumMap<K, V>(K::class.java).also { it.putAll(pairs) }

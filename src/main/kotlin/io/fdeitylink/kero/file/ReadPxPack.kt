1bcd3d8155f439b966b7f49b448f8247d0e80274
fdeitylink <11952260+fdeitylink@users.noreply.github.com>
2018-03-18 09:23:24 -0400

    Refactoring in ReadPxpack

--- src/main/kotlin/io/fdeitylink/kero/file/ReadPxPack.kt
+++ src/main/kotlin/io/fdeitylink/kero/file/ReadPxPack.kt
@@ -2,19 +2,17 @@
 
 package io.fdeitylink.kero.file
 
-import java.util.EnumMap
-
 import java.nio.channels.ReadableByteChannel
 import java.nio.channels.SeekableByteChannel
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
 
-import kotlinx.collections.immutable.immutableListOf
-
 import io.fdeitylink.util.toUInt
 
 import io.fdeitylink.util.Quadruple
 import io.fdeitylink.util.Quintuple
+
+import io.fdeitylink.util.enumMapOf
 
 import io.fdeitylink.kero.CHARSET
 
@@ -31,15 +29,14 @@ import io.fdeitylink.kero.validateName
 
 internal fun PxPack.Companion.fromChannel(chan: SeekableByteChannel): PxPack {
     val head = Head.fromChannel(chan)
-    val layers = EnumMap<TileLayer.Type, TileLayer>(TileLayer.Type::class.java).also { map ->
-        TileLayer.Type.values().forEach { map[it] = TileLayer.fromChannel(chan) }
-    }
+    val layers = enumMapOf(*TileLayer.Type.values().map { Pair(it, TileLayer.fromChannel(chan)) }.toTypedArray())
+
     val numUnits = ByteBuffer.allocate(2).order(ByteOrder.LITTLE_ENDIAN).let {
         chan.read(it)
         it.flip()
         it.getShort().toUInt()
     }
-    val units = immutableListOf(*Array(numUnits) { PxUnit.fromChannel(chan) })
+    val units = List(numUnits) { PxUnit.fromChannel(chan) }
 
     return PxPack(head, layers, units)
 }
@@ -66,19 +63,19 @@ private fun Head.Companion.fromChannel(chan: ReadableByteChannel): Head {
         BackgroundColor(it.get(), it.get(), it.get())
     }
 
-    val layerProperties = EnumMap<TileLayer.Type, LayerProperties>(TileLayer.Type::class.java).also { map ->
-        TileLayer.Type.values().forEach {
-            val tileset = nameFromChannel(chan, "tileset")
+    val layerProperties = enumMapOf(
+            *TileLayer.Type.values().map {
+                val tileset = nameFromChannel(chan, "tileset")
 
-            val (visibilityType, scrollType) = ByteBuffer.allocate(2).let {
-                chan.read(it)
-                it.flip()
-                Pair(VisibilityType(it.get()), ScrollType.values()[it.get().toUInt()])
-            }
+                val (visibilityType, scrollType) = ByteBuffer.allocate(2).let {
+                    chan.read(it)
+                    it.flip()
+                    Pair(VisibilityType(it.get()), ScrollType.values()[it.get().toUInt()])
+                }
 
-            map[it] = LayerProperties(tileset, visibilityType, scrollType)
-        }
-    }
+                Pair(it, LayerProperties(tileset, visibilityType, scrollType))
+            }.toTypedArray()
+    )
 
     return Head(desc, maps, spritesheet, unknownBytes, bgColor, layerProperties)
 }

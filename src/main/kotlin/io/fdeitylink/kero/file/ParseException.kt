639295ad69aa6a88f928c3f526c87884347d508b
fdeitylink <11952260+fdeitylink@users.noreply.github.com>
2018-08-16 09:01:08 -0400

    Added more constructors to ParseException
    Can now pass message, cause, or both

--- src/main/kotlin/io/fdeitylink/kero/file/ParseException.kt
+++ src/main/kotlin/io/fdeitylink/kero/file/ParseException.kt
@@ -16,4 +16,8 @@
 
 package io.fdeitylink.kero.file
 
-internal class ParseException(message: String) : Exception(message)
+internal class ParseException(message: String?, cause: Throwable?) : Exception(message, cause) {
+    constructor(message: String?) : this(message, null)
+
+    constructor(cause: Throwable?) : this(null, cause)
+}

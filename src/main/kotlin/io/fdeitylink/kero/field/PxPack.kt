23c867fdb05fcf70fbf50ec49bf4e0c4ed4ce6b6
fdeitylink <11952260+fdeitylink@users.noreply.github.com>
2018-08-19 01:03:58 -0400

    Changed PxPack to mutable data class w/ a JavaFX property exposed (just units)

--- src/main/kotlin/io/fdeitylink/kero/field/PxPack.kt
+++ src/main/kotlin/io/fdeitylink/kero/field/PxPack.kt
@@ -16,14 +16,26 @@
 
 package io.fdeitylink.kero.field
 
-import kotlinx.collections.immutable.ImmutableList
-import kotlinx.collections.immutable.immutableListOf
-import kotlinx.collections.immutable.toImmutableList
+import javafx.collections.ObservableList
 
-import io.fdeitylink.util.toEnumMap
+import javafx.beans.property.ReadOnlyListProperty
+import javafx.beans.property.ReadOnlyListWrapper
+
+import tornadofx.observableList
+import tornadofx.observable
+
+import io.fdeitylink.util.enumMapOf
+
+import io.fdeitylink.util.validate
 
 /**
  * Represents a PxPack field
+ *
+ * @constructor
+ * Constructs a new [PxPack] object
+ *
+ * @throws [IllegalArgumentException] if [layers] is invalid as per [isValidLayers],
+ * or [units] is invalid as per [isValidUnits]
  */
 internal data class PxPack(
         /**
@@ -35,26 +47,62 @@ internal data class PxPack(
         /**
          * Represents the [tile layers][TileLayer] of this PxPack field
          */
-        val layers: Map<TileLayer.Type, TileLayer> = TileLayer.Type.values().associate { it to TileLayer() }.toEnumMap(),
+        val layers: Map<TileLayer.Type, TileLayer> =
+                TileLayer.Type.values().associateTo(enumMapOf<TileLayer.Type, TileLayer>()) { it to TileLayer() },
 
         /**
          * Represents the [units][PxUnit] of this PxPack field
          */
-        val units: ImmutableList<PxUnit> = immutableListOf()
+        val units: ObservableList<PxUnit> = observableList()
 ) {
     init {
-        require(layers.size == TileLayer.NUMBER_OF_TILE_LAYERS)
-        { "layers.size != ${TileLayer.NUMBER_OF_TILE_LAYERS} (size: ${layers.size})" }
-
-        require(units.size <= PxUnit.MAXIMUM_NUMBER_OF_UNITS)
-        { "units.size must be <= ${PxUnit.MAXIMUM_NUMBER_OF_UNITS} (size: ${units.size})" }
+        validateLayers(layers)
+        validateUnits(units)
     }
+
+    val unitsProperty: ReadOnlyListProperty<PxUnit> = ReadOnlyListWrapper(this.units)
 
     constructor(
             head: Head = Head(),
-            layers: Map<TileLayer.Type, TileLayer> = TileLayer.Type.values().associate { it to TileLayer() }.toEnumMap(),
-            units: List<PxUnit> = immutableListOf()
-    ) : this(head, layers, units.toImmutableList())
+            layers: Map<TileLayer.Type, TileLayer> =
+                    TileLayer.Type.values().associateTo(enumMapOf<TileLayer.Type, TileLayer>()) { it to TileLayer() },
+            units: List<PxUnit> = listOf()
+    ) : this(head, layers, units.toMutableList().observable())
 
-    companion object
+    companion object {
+        /**
+         * Returns `true` if the [size][Map.size] of `this` set of [TileLayer] objects contains one instance for every
+         * layer in a PxPack field, `false` otherwise
+         */
+        fun Map<TileLayer.Type, TileLayer>.isValidLayers() = this.size == TileLayer.NUMBER_OF_TILE_LAYERS
+
+        /**
+         * Constructs and throws an exception (using [exceptCtor]) if [layers] does not contain an object for every
+         * tile layer in a PxPack field
+         *
+         * @param exceptCtor Defaults to the [IllegalArgumentException] constructor
+         */
+        fun validateLayers(
+                layers: Map<TileLayer.Type, TileLayer>,
+                exceptCtor: (String) -> Exception = ::IllegalArgumentException
+        ) =
+                validate(layers.size == TileLayer.NUMBER_OF_TILE_LAYERS, exceptCtor)
+                { "layers.size != ${TileLayer.NUMBER_OF_TILE_LAYERS} (size: ${layers.size})" }
+
+        /**
+         * Returns `true` if the [size][List.size] of `this` list of [PxUnit] objects does not exceed
+         * [PxUnit.MAXIMUM_NUMBER_OF_UNITS], `false` otherwise
+         */
+        fun List<PxUnit>.isValidUnits() = this.size <= PxUnit.MAXIMUM_NUMBER_OF_UNITS
+
+        /**
+         * Constructs and throws an exception (using [exceptCtor]) if the [size][List.size] of [units] exceeds
+         * [PxUnit.MAXIMUM_NUMBER_OF_UNITS]
+         *
+         * @param exceptCtor Defaults to the [IllegalArgumentException] constructor
+         */
+        fun validateUnits(units: List<PxUnit>, exceptCtor: (String) -> Exception = ::IllegalArgumentException) =
+                validate(units.size <= PxUnit.MAXIMUM_NUMBER_OF_UNITS, exceptCtor)
+                { "units.size must be <= ${PxUnit.MAXIMUM_NUMBER_OF_UNITS} (size: ${units.size})" }
+    }
 }
